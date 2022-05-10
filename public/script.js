5019be214135c4e6ce182fe6dc4c95ef7e6042f9
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2022-05-10 13:29:49 +0200

    dragging images not working

--- public/script.js
+++ public/script.js
@@ -1,30 +1,84 @@
 let socket = io()
 let canvas
-let eraseEnable = false
+const results = document.querySelectorAll('.results li img')
+let source
+let context
+const penBtn = document.querySelector('.pen');
+let imgPositionX = 143
+let imgPositionY = 154
+let data
 
 function setup() {
     createCanvas(710, 400);
     background(230, 0);
-
+    canvas = document.querySelector('canvas');
+    context = canvas.getContext('2d');
     socket.on('mouse', (data) =>  {
         stroke(000);
         line(data.x , data.y , data.pX, data.pY);
     });
 }
-  
+
 function draw() {
-    let data = {
+    data = {
         x: mouseX,
         y: mouseY,
         pX: pmouseX,
         pY: pmouseY,
     }
-
-
-    if(mouseIsPressed === true) {
+    
+    if(mouseIsPressed === true && penBtn.classList.contains('active')) {
         stroke(000);
         line(data.x , data.y , data.pX, data.pY);
         socket.emit('mouse', data)
     }
 }
-  
+
+
+
+penBtn.addEventListener('click', () => {
+    if(penBtn.classList.contains('active')) {
+        penBtn.classList.remove('active')
+    } else {
+        penBtn.classList.add('active')
+    }
+})
+
+results.forEach(result => {
+    result.addEventListener('click', () => {
+        const source = result.src
+        const regularSize = source.slice(0, -3) + 1080
+        const img = new Image();
+        
+        img.src = regularSize
+        img.onload = () => {
+            let scaledWidth = img.width / 10
+            let scaledHeight = img.height / 10
+            context.drawImage(img, imgPositionX, imgPositionY, scaledWidth, scaledHeight)
+            socket.emit('image', source)
+            mouseOverImages(scaledWidth, scaledHeight)
+        }
+    })
+})
+
+function mouseOverImages(scaledWidth, scaledHeight) {
+    const surfaceX = imgPositionX + scaledWidth
+    const surfaceY = imgPositionY + scaledHeight
+
+    canvas.addEventListener('mousemove', (e) => {
+        if(imgPositionX < data.x && surfaceX > data.x && imgPositionY < data.y && surfaceY > data.y) {
+            
+        }
+    })
+}
+
+
+socket.on('image', (source) =>  {
+    const regularSize = source.slice(0, -3) + 1080
+    const img = new Image();
+
+    img.src = regularSize
+    img.onload = () => {
+        context.drawImage(img, imgPositionX, imgPositionY, img.width / 10, img.height / 10)
+    }
+});
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+const apiKey = process.env.KEY
+
 const port = 2022
 
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
 app.use(express.static(path.resolve('public')))
+
+app.get('/', function (req, res) {
+  const key = `https://api.unsplash.com/search/photos/?client_id=${apiKey}&query=${req.query.q}`
+
+  console.log(key);
+  fetch(key)
+    .then(async response => {
+      const data = await response.json()
+      const results = data.results
+      console.log(results);
+
+      res.render('index', {
+        results
+      })
+    } 
+    )
+})
+
 
 io.on('connection', (socket) => {
   console.log('a user connected')
@@ -18,19 +43,14 @@ io.on('connection', (socket) => {
     console.log('user disconnected')
   })
 
-  // socket.on('mouse', (data) => {
-  //   console.log('iets');
-  // })
-
-
   socket.on('mouse', (data) =>  {
-    // Data comes in as whatever was sent, including objects
-    // console.log("Received: 'mouse' " + data.x + " " + data.y);  
-    // console.log(data);    
-    // Send it to all other clients
     socket.broadcast.emit('mouse', data);
-  }
-);
+  })
+
+  socket.on('image', (source) =>  {
+    socket.broadcast.emit('image', source);
+  })
+  
 })
 
 
